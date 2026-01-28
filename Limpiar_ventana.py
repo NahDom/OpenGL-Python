085f12af30c9267a2ae0a5bf797b10c1e5d66074
Galatic Pizza <Clastik-mor13@outlook.com.ar>
2026-01-28 14:54:49 -0300

    Adhiero estructura para el renderizador de shaders

--- Limpiar_ventana.py
+++ Limpiar_ventana.py
@@ -1,6 +1,3 @@
-"""
-    Primer prueba limpiar una pantalla
-"""
 #region imports
 import pygame as pg
 from OpenGL.GL import *
@@ -17,17 +14,62 @@ SCREEN_COLOR = (0.5,0.0,0.25,1.0)
 WINDOW_CREATION_FLAGS = pg.OPENGL | pg.DOUBLEBUF # doble buffer para la ventana
 FRAMERATE = 60
 #endregion
+
+#region shader
+def make_shader(vertex_filename: str, fragment_filename: str) -> int:
+    vertex_module = make_shader_module(vertex_filename, GL_VERTEX_SHADER)
+    fragment_module = make_shader_module(fragment_filename, GL_FRAGMENT_SHADER)
+    return compileProgram(vertex_module, fragment_module)
+    # debo vincular para generar el binario
+    
+
 def make_shader_module(filename: str, module_type: int) -> int:
     # cargamos el archivo de los vertices y fragmentos
     with open(filename, "r") as file:
+        source_code = file.readlines()
+    return compileShader(source_code, module_type)
+        # extraigo el codigo y le indico a la gpu que lo compile, 
+
+class Renderer:
+    SCREEN_COLOR = (0.5,0.0,0.25,1.0)
+    
+    def __init__(self):
+        glClearColor(*Renderer.SCREEN_COLOR)
+        # accedo a la variable estatica del color que se debe de renderizar
+        
+        self.VAO = glGenVertexArrays(1)
+        glBindVertexArray(self.VAO)
+
+        self.shader = make_shader("vertex.txt", "fragment.txt")
+        
+    def draw(self) -> None:
+        
+        glClear(GL_COLOR_BUFFER_BIT)
+
+        glUseProgram(self.shader)
+        glDrawArrays(GL_TRIANGLES, 0, 3)
+        
+    def destroy(self) -> None:
+        
+        glDeleteProgram(self.shader)  
+#end shader
 #region setup
+
+
 # lo mas recomendable para que sea constante el framerate
 pg.init()
+
+# para el control de versiones de OpenGL
+pg.display.gl_set_attribute(pg.GL_CONTEXT_MAJOR_VERSION, 3)
+pg.display.gl_set_attribute(pg.GL_CONTEXT_MINOR_VERSION, 3)
+pg.display.gl_set_attribute(pg.GL_CONTEXT_PROFILE_MASK, pg.GL_CONTEXT_PROFILE_CORE)
+
 # creo la ventana
 screen = pg.display.set_mode(SCREEN_SIZE, WINDOW_CREATION_FLAGS)
 clock = pg.time.Clock()
+renderer = Renderer()
 # en python recordar que se puede descomprimir los elementos dentro de una lista o tupla con el operador *lista | *tupla 
-glClearColor(*SCREEN_COLOR)
+
 #endregion
 
 #region main loop
@@ -35,10 +77,12 @@ running = True
 while running:
     #lista de eventos ocurridos desde la ultima llamada
     for event in pg.event.get():
-        if (event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.type == pg.K_ESCAPE)):
+        if event.type == pg.QUIT:
             running = False
-        
-    glClear(GL_COLOR_BUFFER_BIT)
+        if event.type == pg.KEYDOWN:
+            if event.key == pg.K_ESCAPE:
+                running = False
+    renderer.draw()
     #invierto los fotogramas para mostrar los colores
     pg.display.flip()
     #actualizo el reloj interno
@@ -47,5 +91,6 @@ while running:
 #endregion
 
 #region cleanup
+renderer.destroy()
 pg.quit()
 #endregion
